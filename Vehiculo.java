e7633b630525af642d94c6a3bc13dc0703741f14
Deniss3232 <iqdenis@gmail.com>
2025-05-06 21:45:45 -0600

    Add files via upload

---
+++ Vehiculo.java
@@ -0,0 +1,9 @@
+public class Vehiculo {
+    private String nombre;
+    private String marca;
+    private double precio;
+    private String tipo; // Ej: "SUV", "Pickup", "Compacto"
+    private List<String> caracteristicas; // Ej: "4x4", "bajo consumo", "eléctrico"
+
+    // Getters, setters, constructor
+}
