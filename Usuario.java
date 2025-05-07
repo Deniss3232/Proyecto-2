3bd11c83f2a03551ad05167532401a30951dd194
VirusMauri1 <san231977@uvg.edu.gt>
2025-05-06 23:27:32 -0600

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    private String nombre;
+    private String estiloDeVida; // Ej: "urbano", "rural", "familiar"
+    private double presupuesto;
+    private List<String> preferencias; // características deseadas
+
+    // Getters, setters, constructor
+}
