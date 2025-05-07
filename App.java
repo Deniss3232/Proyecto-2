5522293f29b40131db34a0e74eadda1662bb0f97
VirusMauri1 <san231977@uvg.edu.gt>
2025-05-06 23:28:08 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        // Crear usuario de prueba
+        // Conectar a Neo4j
+        // Llamar al ServicioRecomendacion
+        // Imprimir resultados
+    }
+}
