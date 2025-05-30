8cf096f53671f4e2ddb4f04eb93db655d2d3034c
VirusMauri1 <san231977@uvg.edu.gt>
2025-05-29 22:03:08 -0600

    Update ConexionNeo4j.java

--- ConexionNeo4j.java
+++ ConexionNeo4j.java
@@ -1,7 +1,23 @@
-public class ConexionNeo4j {
-    private Driver driver;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
 
-    public void conectar();
-    public void cerrar();
-    public Session obtenerSesion();
+public class ConexionNeo4j {
+    private final Driver driver;
+
+    // Constructor para conectar al servidor Neo4j
+    public ConexionNeo4j(String uri, String usuario, String contrasena) {
+        this.driver = GraphDatabase.driver(uri, AuthTokens.basic(usuario, contrasena)); // Establece la conexión
+    }
+
+    // Abre una nueva sesión
+    public Session obtenerSesion() {
+        return driver.session(); // Devuelve una sesión para ejecutar queries
+    }
+
+    // Cierra la conexión
+    public void cerrar() {
+        driver.close(); // Finaliza la conexión con la base de datos
+    }
 }
