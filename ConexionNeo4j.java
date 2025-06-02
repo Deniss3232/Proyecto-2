21ce0c1da6f57bcfadb3bdea689e5cd8cb33da61
Deniss3232 <iqdenis@gmail.com>
2025-06-01 23:39:20 -0600

    Update ConexionNeo4j.java

--- ConexionNeo4j.java
+++ ConexionNeo4j.java
@@ -6,18 +6,17 @@ import org.neo4j.driver.Session;
 public class ConexionNeo4j {
     private final Driver driver;
 
-    // Constructor para conectar al servidor Neo4j
     public ConexionNeo4j(String uri, String usuario, String contrasena) {
-        this.driver = GraphDatabase.driver(uri, AuthTokens.basic(usuario, contrasena)); // Establece la conexión
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(usuario, contrasena));
     }
 
-    // Abre una nueva sesión
     public Session obtenerSesion() {
-        return driver.session(); // Devuelve una sesión para ejecutar queries
+        return driver.session();
     }
 
-    // Cierra la conexión
     public void cerrar() {
-        driver.close(); // Finaliza la conexión con la base de datos
+        if (driver != null) {
+            driver.close();
+        }
     }
 }
